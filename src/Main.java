37f747cd2a4ffe7ec7366e4eded3f83a45f959f3
VladimirM1 <steam.vova4@gmail.com>
2023-03-13 22:00:56 +0200

    SõneLugeja täiendus
    sõnelugeja täiendus, testsõna.txt fail ja väike test mainis

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        //SõneLugeja kontroll
+        SõneLugeja a = new SõneLugeja("src/testSõnad.txt");
+        System.out.println(a.arvatavSõna());
+
     }
 
 }
--- src/SõneLugeja.java
+++ src/SõneLugeja.java
@@ -1,2 +1,30 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class SõneLugeja {
+    private String failitee;
+
+
+    public SõneLugeja(String failitee){
+        this.failitee = failitee;
+    }
+
+    public String[] loeFail() throws IOException {
+        List<String> read = Files.readAllLines(Path.of(failitee), StandardCharsets.UTF_8);
+        System.out.println(read);
+        String[] sõnadeMassiiv = new String[read.size()];
+        for (int i = 0; i < sõnadeMassiiv.length; i++) {
+            sõnadeMassiiv[i] = read.get(i);
+        }
+        return sõnadeMassiiv;
+    }
+    public String arvatavSõna() throws IOException {
+        String[] sõnadeMassiiv = loeFail();
+        int suvalineArv = (int) Math.round((Math.random() * sõnadeMassiiv.length));
+        return sõnadeMassiiv[suvalineArv];
+    }
+
 }
