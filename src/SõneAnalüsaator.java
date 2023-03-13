7788a164908a29227b2cd4ae51227ebfa15b53b3
rops11 <robertivask@gmail.com>
2023-03-13 21:03:42 +0200

    SõneAnalüsaator on valmis

--- src/SõneAnalüsaator.java
+++ src/SõneAnalüsaator.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class SõneAnalüsaator {
     //Insediväljad
     private String õigeVastus;
@@ -8,7 +11,25 @@ public class SõneAnalüsaator {
     }
 
     //Meetodid
+    public Map<Character, Integer> unikaalseteSümboliteArv(String sõne) {
+        HashMap<Character, Integer> tagasta = new HashMap<>();
+
+        for (int i = 0; i < sõne.length(); i++) {
+            char täht = sõne.charAt(i);
+            if (tagasta.containsKey(täht)) {
+                tagasta.put(täht, tagasta.get(täht) + 1);
+            } else {
+                tagasta.put(täht, 1);
+            }
+        }
+
+        return tagasta;
+    }
     public boolean kontrolliVastust(String pakumine) {
+        //Kuvab suurt tähte, kui täht on õiges kohas
+        //Kuvab väikest tähte, kui sisaldab tähte, aga täht on vales kohas
+        //Kuvab "_", kui tähte ei ole üldse sõnas või samu tähti on üleliia.
+
         pakumine = pakumine.toLowerCase();
 
         //Kontrollime, et sõne pikkus oleks sama, mis vastusel
@@ -26,9 +47,24 @@ public class SõneAnalüsaator {
         String vihje = "";
         String[] tähedPakumine = pakumine.split("");
         String[] tähedÕigeVastus = õigeVastus.split("");
+        Map<Character, Integer> uniaalseteSümboliteArvVastus = unikaalseteSümboliteArv(õigeVastus);
+
         for (int i = 0; i < tähedPakumine.length; i++) {
+            char otsitavSümbol = tähedPakumine[i].charAt(0);
+
             if (tähedPakumine[i].equals(tähedÕigeVastus[i])) {
-                vihje += tähedPakumine[i];
+                vihje += tähedPakumine[i].toUpperCase();
+                uniaalseteSümboliteArvVastus.put(otsitavSümbol, uniaalseteSümboliteArvVastus.get(otsitavSümbol) - 1);
+                if (uniaalseteSümboliteArvVastus.get(otsitavSümbol) < 1) {
+                    uniaalseteSümboliteArvVastus.remove(otsitavSümbol);
+                }
+            }
+            else if (uniaalseteSümboliteArvVastus.containsKey(otsitavSümbol)) {
+                vihje += tähedPakumine[i].toLowerCase();
+                uniaalseteSümboliteArvVastus.put(otsitavSümbol, uniaalseteSümboliteArvVastus.get(otsitavSümbol) - 1);
+                if (uniaalseteSümboliteArvVastus.get(otsitavSümbol) < 1) {
+                    uniaalseteSümboliteArvVastus.remove(otsitavSümbol);
+                }
             }
             else {
                 vihje += "_";
@@ -40,10 +76,11 @@ public class SõneAnalüsaator {
         return false;
     }
 
+
     /*test
     public static void main(String[] args) {
         SõneAnalüsaator sõneAnalüsaator = new SõneAnalüsaator("koer");
-        System.out.println(sõneAnalüsaator.kontrolliVastust("kass"));
+        System.out.println(sõneAnalüsaator.kontrolliVastust("kaks"));
     }
     */
 }
